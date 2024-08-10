41d6c2311af75fd40ffe6b4b8accafe2ac41fb6d
Renil Raj <renilraj333@gmail.com>
2024-08-10 16:02:09 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use rand::{seq::SliceRandom, thread_rng};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = ["Ace", "Two", "Three", "Four", "Five", "Six"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card)
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng)
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+
+    deck.shuffle();
+
+    let cards = deck.deal(3);
+
+    println!("Here is your deck: {:#?}", deck);
+
+    println!("Here is your hand: {:#?}", cards);
+}
